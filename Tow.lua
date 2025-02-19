f7dabacd9286a456d30d2bffd38acd5394bfbe74
Obisedor <obisedor1@outlook.com>
2025-02-19 11:00:25 +0100

    Update Tow.lua

--- Tow.lua
+++ Tow.lua
@@ -1,3 +1,25 @@
+local function SendMessageEMBED(url, embed)
+    local http = game:GetService("HttpService")
+    local headers = {
+        ["Content-Type"] = "application/json"
+    }
+    local data = {
+        ["embeds"] = {
+            {
+                ["title"] = embed.title or "",
+                ["description"] = embed.description or "",
+            }
+        }
+    }
+    local body = http:JSONEncode(data)
+    local response = request({
+        Url = url,
+        Method = "POST",
+        Headers = headers,
+        Body = body
+    })
+end
+
 local TargetVehicle = "Caddy"
 local TowTruck = game.Workspace.PlayerVehicles["Tow Truck"]
 
@@ -9,11 +31,13 @@ local Vehicles = {}
 for i, v in pairs(game.Workspace.PlayerVehicles:GetChildren()) do
     if v.Name == TargetVehicle then
         Vehicles[v] = {
-            Locked = false,
+            Locked = v.VehicleSeat.CarLocked.Value,
             LockDebounce = false
         }
     end
-    Events.LockCar:FireServer(v)
+    if not v.VehicleSeat.CarLocked.Value then
+        Events.LockCar:FireServer(v)
+    end
 end
 
 task.wait(1.5)
@@ -21,31 +45,46 @@ task.wait(1.5)
 while true do
     for i, v in pairs(Vehicles) do
         if not v.Locked and not v.LockDebounce then
+            local Failed
+            local FailedSpot
+
             Events.EnterVehicle:FireServer(i, "FrontLeft")
             repeat task.wait() until LP.Character and LP.Character:FindFirstChild("Humanoid") and LP.Character.Humanoid.SeatPart
+
             LP.Character.Humanoid.SeatPart.Parent:SetPrimaryPartCFrame(CFrame.new(431, -4, -1777))
 			task.wait(.05)
             LP.Character.Humanoid.SeatPart.Parent:SetPrimaryPartCFrame(CFrame.new(431, -4, -1777))
             wait(0.65)
+
             LP.Character.Humanoid.Jump = true
             repeat task.wait() until not LP.Character.Humanoid.SeatPart
             wait(0.1)
 
             LP.Character.HumanoidRootPart.CFrame = CFrame.new(395, -2, -1786)
-            repeat task.wait() until (LP.Character.HumanoidRootPart.Position - Vector3.new(395, -2, -1786)).Magnitude < 10
             wait(0.65)
 
             Events.GetTowingTool:FireServer(TowTruck)
             repeat task.wait() until LP.Character:FindFirstChild("Tow Car")
+
+            local Timeout = 0
             Events.TowCar:FireServer(TowTruck, "Tow", i)
-            repeat task.wait() until i.VehicleSeat.Towed.Value and TowTruck.VehicleSeat.TowingVehicle.Value
+            repeat task.wait(.05) Timeout = Timeout + .05 until i.VehicleSeat.Towed.Value and TowTruck.VehicleSeat.TowingVehicle.Value or Timeout.Value >= 1
+            if Timeout >= 1 then Failed = true FailedSpot = "Towing" end
+
+            local Timeout = 0
             Events.TowCar:FireServer(TowTruck, "Release", i)
-            repeat task.wait() until not i.VehicleSeat.Towed.Value and not TowTruck.VehicleSeat.TowingVehicle.Value
-            v.Locked = true
-            v.LockDebounce = true
-	    	task.wait(.25)
-            Events.LockCar:FireServer(i)
-            task.spawn(function() task.wait(1.5) v.Locked = false v.LockDebounce = false end)
+            repeat task.wait(.05) Timeout = Timeout + .05 until not i.VehicleSeat.Towed.Value and not TowTruck.VehicleSeat.TowingVehicle.Value or Timeout >= 1
+            if Timeout >= 1 then Failed = true FailedSpot = "Release" end
+
+            if not Failed then
+                v.Locked = true
+                v.LockDebounce = true
+                task.wait(.15)
+                Events.LockCar:FireServer(i)
+                task.spawn(function() task.wait(1.5) v.Locked = false v.LockDebounce = false end)
+            else
+                SendMessageEMBED(https://discord.com/api/webhooks/1341709504924094474/7i0_3-5ZZWEPO-V0DoTAFIosXUCNnjhVbWIKq7co-OgARmRodD8-8ICg5d5XNpPQSTzr, {Title = "ERROR", Description = "Error occured in: " .. FailedSpot or "unknown"})
+            end
         end
     end
 	task.wait()
