53dedf86139b3741699935f4e1b37b09a47c64bb
Obisedor <obisedor1@outlook.com>
2026-03-06 15:12:44 +0100

    Update GiveMoney.lua

--- GiveMoney.lua
+++ GiveMoney.lua
@@ -1,28 +1,27 @@
-local TargetPlayer = "" -- first few characters
--- AMOUNT IN MILLIONS!
-local Amount = 10
+local TargetPlayer = game.Players:FindFirstChild("Username")
+local Amount = 10 -- In millions
 
-local Events = game:GetService("ReplicatedStorage"):WaitForChild("_CS.Events")
-local LeftAmount = Amount * 1000000
-
-local function findPlayerByPartialName(partialName)
-    partialName = string.lower(partialName) -- Make search case-insensitive
-    for _, player in ipairs(game.Players:GetPlayers()) do
-        if string.sub(string.lower(player.Name), 1, #partialName) == partialName then
-            return player
-        end
-    end
-    return nil -- No matching player found
+if not TargetPlayer then 
+    print("-----------------------")
+    error("Target player not found")
+    print("-----------------------")
+    return
 end
 
-repeat
-    Events.GiveMoneyToPlr:FireServer()
-    if LeftAmount > 250000 then
-        Events.GiveMoneyToPlr:FireServer(findPlayerByPartialName(TargetPlayer), "250000")
-        LeftAmount = LeftAmount - 250000
+local Events = game:GetService("ReplicatedStorage"):WaitForChild("_CS.Events")
+
+local AmountLeft = Amount * 1000000
+local TimeLeft = (math.roof(AmountLeft / 250000)) * 1.025
+while true do
+    if AmountLeft == 0 then return end
+    local AmountToGive
+    if AmountLeft <= 250000 then
+        AmountToGive = 250000
     else
-        Events.GiveMoneyToPlr:FireServer(findPlayerByPartialName(TargetPlayer), tostring(LeftAmount))
-        LeftAmount = 0
+        AmountToGive = AmountLeft
     end
-    task.wait(11.5)
-until LeftAmount == 0 
+    AmountLeft = AmountLeft - AmountToGive
+    TimeLeft = TimeLeft - 10.25
+    Events.GiveMoneyToPlr:FireServer(TargetPlr, AmountToGive)
+    task.wait(10.25)
+end
